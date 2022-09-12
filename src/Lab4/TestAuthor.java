c7c575a98cd622c8a51a3e85cbb24637e5fc1212
Rudnev Viktor <rudnevvictor2003@mail.ru>
2022-09-13 01:02:44 +0300

    4 lab 50%

---
+++ src/Lab4/TestAuthor.java
@@ -0,0 +1,14 @@
+package Lab4;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("John", "John1999@mail.ru", 'm');
+        System.out.print("For author:\n" +
+                "toString: " + a.toString() + '\n' +
+                "set email (jason@mail.ru) and get it: ");
+        a.setEmail("jason@mail.ru");
+        System.out.print(a.getEmail() + '\n');
+        System.out.print("Get name: " + a.getName() + '\n' +
+                "Get gender: " + a.getGender());
+    }
+}
