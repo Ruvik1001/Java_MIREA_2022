d2b477afdf607deaac1df8b69c2dca892b965f66
Rudnev Viktor <rudnevvictor2003@mail.ru>
2022-09-15 13:08:32 +0300

    4.1 2/10

--- src/Lab4_1/Phones/Phone.java
+++ src/Lab4_1/Phones/Phone.java
@@ -1,2 +1,46 @@
-package Lab4_1.Phones;public class Phone {
+package Lab4_1.Phones;
+
+public class Phone {
+    private String number = "+7(908)542-57-42";
+    private String model = "Poco M3 Pro";
+    private float weight = 0.38f;
+    //////////////////////////////////
+    public Phone() {}
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(String number, String model, float weight) {
+        this(number, model);
+        this.weight = weight;
+    }
+    //////////////////////////////////
+
+    public void receiveCall(String number) {
+        System.out.print("Incoming call from " + number + '\n');
+    }
+
+    public void receiveCall(String name, String number) {
+        receiveCall('*' + name + "* " + number);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void sendMessage(String... numbers) {
+        System.out.print("Msg was send:\n");
+        for (String num : numbers) System.out.println(num);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "number='" + number + '\'' +
+                ", model='" + model + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
--- src/Lab4_1/Phones/Tester.java
+++ src/Lab4_1/Phones/Tester.java
@@ -1,2 +1,40 @@
-package Lab4_1.Phones;public class Tester {
+package Lab4_1.Phones;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String... args) {
+        Phone p1 = new Phone();
+        Phone p2 = new Phone("+7(987)833-88-12", "Redmi 9C NFC");
+        Phone p3 = new Phone("+7(915)108-55-25", "Real Me 9A", 0.23f);
+
+        System.out.print(
+                "Some info about phones with different parameters:\n" +
+                p1.toString() + '\n' +
+                p2.toString() + '\n' +
+                p3.toString() + '\n');
+        System.out.print(
+                "\nMethod receiveCall: ");
+        p1.receiveCall("+7(987)823-12-24");
+        System.out.print(
+                "Method with name: ");
+        p1.receiveCall("Dimon", "+7(908)542-24-32");
+
+        System.out.print(
+                "\nMethod getNumber for all phones:\n" +
+                p1.getNumber() + '\n' +
+                p2.getNumber() + '\n' +
+                p3.getNumber() + '\n' + '\n');
+
+        System.out.print(
+                "Input count of numbers for msg: ");
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        String[] num = new String[count];
+        System.out.print("Input " + count + " numbers\n");
+        for (int i = 0; i < count; i++) {
+            num[i] = in.next();
+        }
+        p1.sendMessage(num);
+    }
 }
